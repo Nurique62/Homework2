157f589451f7994ab713f82a9f91149e9a7068f0
Nurlan <Nurique62@gmail.com>
2022-05-23 11:34:09 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 1000;
+        int rubles = 20;
+
+        System.out.println("Вам начислено бонусных миль:");
+        System.out.println(amount / rubles);
+    }
+}
